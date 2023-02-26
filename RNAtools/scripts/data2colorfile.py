76343e359698c4e7619eef84e80b7aef90a5277c
grice <grice@users.noreply.github.com>
2023-02-26 15:32:04 -0500

    added shape coloring to colorfile generator

--- RNAtools/scripts/data2colorfile.py
+++ RNAtools/scripts/data2colorfile.py
@@ -25,23 +25,39 @@ def parseArgs():
                      help='Max value for color scale')
     prs.add_argument('--twoCol', action="store_true",
                      help="input is two column .shape format")
+    prs.add_argument('--shape', action="store_true",
+                     help="input is two column and use shape colorscale")
 
     # parse and return
     out=prs.parse_args()
 
-    return out.infile, out.outfile, out.palette, out.vmin, out.vmax, out.twoCol
+    return out.infile, out.outfile, out.palette, out.vmin, out.vmax, out.twoCol, out.shape
 
 def main():
 
-    infile, outfile, palette, vmin, vmax, twoCol = parseArgs()
+    infile, outfile, palette, vmin, vmax, twoCol, shape = parseArgs()
 
+    # define colors for shape palette, norm data -1 to 1
+    shapepal = [(0,"#c5c5c5"), #gray
+           (0.001, "#000000"), #black
+           (0.695,"#000000"),
+           (0.7,"#f6a429"), # yellow
+           (0.924,"#f6a429"),
+           (0.925,"#f60c1b"), #red
+           (1,"#f60c1b")]
     if twoCol:
+        data = readSHAPE(infile)
+    elif shape:
         data = readSHAPE(infile)
     else:
         data = dotPlot(infile).calcShannon()
     
     norm = colors.Normalize(vmin=vmin, vmax=vmax)
     cmap = cm.get_cmap(palette)
+
+    if shape:
+        norm = colors.Normalize(vmin=-1, vmax=1)
+        cmap = colors.LinearSegmentedColormap.from_list("shapecolors", shapepal)
 
     line = ""
     for i, val in enumerate(data):
